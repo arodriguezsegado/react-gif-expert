509a36b29900026ab4f346ee34b4d98a183c705c
Alejandro Rodriguez <arodriguezsegado@gmail.com>
2022-09-08 20:01:40 +0200

    Primer commit

---
+++ src/components/GifGridItem.jsx
@@ -0,0 +1,14 @@
+
+
+export const GifGridItem = ({ title, url }) => {
+    
+    // console.log(url)
+    return (
+        <div className="card">
+            <img src={ url } alt={ title } />
+            <p>{ title }</p>
+        </div>
+    )
+
+
+}
